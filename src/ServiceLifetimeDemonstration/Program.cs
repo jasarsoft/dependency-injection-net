47e715ed5bdc89a0f489459343273dd850077fed
Edin Jašarević <edinjasar14@gmail.com>
2023-02-24 11:20:29 +0100

    transiet services

--- src/ServiceLifetimeDemonstration/Program.cs
+++ src/ServiceLifetimeDemonstration/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<IGuidService, GuidService>();
 
 var app = builder.Build();
 
