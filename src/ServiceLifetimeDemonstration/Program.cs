ead1c1b5b0dc51ce8a93e4b2feee21fc6c70963c
Edin Jašarević <edinjasar14@gmail.com>
2023-02-24 11:35:07 +0100

    avoiding captive dependecies

--- src/ServiceLifetimeDemonstration/Program.cs
+++ src/ServiceLifetimeDemonstration/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IGuidService, GuidService>();
+builder.Services.AddSingleton<IGuidTrimmer, GuidTrimmer>();
 
 var app = builder.Build();
 
